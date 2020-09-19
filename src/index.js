d46a16dd1542d59d5d9494e5d125654c420cc232
evillt <ijoec123@gmail.com>
2020-09-19 11:02:34 +0800

    refactor: options

--- src/index.js
+++ src/index.js
@@ -1,71 +1,75 @@
-export default (options = {}) => ({
-  name: 'saika-plugin-disqus',
+export default (options = {}) => {
+  options = Object.assign({ enablePaths: ['/posts'] }, options)
 
-  when: () => {
-    if (!options.shortname) {
-      console.error('[saika disqus]: Missing required option `shortname`')
-    }
+  return {
+    name: 'saika-plugin-disqus',
 
-    return options.shortname
-  },
-
-  extend: api => {
-    api.registerComponent('content:end', {
-      render: h => {
-        return h('div', {
-          attrs: { id: 'disqus_thread' },
-          style: { marginTop: '20px' }
-        })
-      },
-
-      mounted() {
-        if (!checkEnable()) return
-
-        if (window.DISQUS) {
-          reset()
-          return
-        }
-
-        init()
-      }
-    })
-
-    const routePath = api.router.currentRoute.path
-    const page = {
-      url: window.location.origin + routePath,
-      identifier: routePath,
-      title: api.store.state.postLinkMeta.title
-    }
-
-    function init() {
-      window.disqus_config = function() {
-        this.page = Object.assign({}, this.page, page)
+    when: () => {
+      if (!options.shortname) {
+        console.error('[saika disqus]: Missing required option `shortname`')
       }
 
-      const script = document.createElement('script')
-      script.src = `https://${options.shortname}.disqus.com/embed.js`
-      script.async = true
-      script.setAttribute('data-timestamp', Date.now())
-      document.body.appendChild(script)
-    }
+      return options.shortname
+    },
 
-    function reset() {
-      window.DISQUS.reset({
-        reload: true,
-        config() {
-          this.page = Object.assign({}, this.page, page)
+    extend: api => {
+      api.registerComponent('content:end', {
+        render: h => {
+          return h('div', {
+            attrs: { id: 'disqus_thread' },
+            style: { marginTop: '20px' }
+          })
+        },
+
+        mounted() {
+          if (!checkEnable()) return
+
+          if (window.DISQUS) {
+            reset()
+            return
+          }
+
+          init()
         }
       })
-    }
 
-    function checkEnable() {
-      const enablePaths = options.enablePaths || ['/posts']
-
-      for (let i = 0; i < enablePaths.length; i++) {
-        if (routePath.indexOf(enablePaths[i]) !== -1) return true
+      const routePath = api.router.currentRoute.path
+      const page = {
+        url: window.location.origin + routePath,
+        identifier: routePath,
+        title: api.store.state.postLinkMeta.title
       }
 
-      return false
+      function init() {
+        window.disqus_config = function() {
+          this.page = Object.assign({}, this.page, page)
+        }
+
+        const script = document.createElement('script')
+        script.src = `https://${options.shortname}.disqus.com/embed.js`
+        script.async = true
+        script.setAttribute('data-timestamp', Date.now())
+        document.body.appendChild(script)
+      }
+
+      function reset() {
+        window.DISQUS.reset({
+          reload: true,
+          config() {
+            this.page = Object.assign({}, this.page, page)
+          }
+        })
+      }
+
+      function checkEnable() {
+        const enablePaths = options.enablePaths
+
+        for (let i = 0; i < enablePaths.length; i++) {
+          if (routePath.indexOf(enablePaths[i]) !== -1) return true
+        }
+
+        return false
+      }
     }
   }
-})
+}
